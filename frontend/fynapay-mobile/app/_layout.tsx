fc4a0be5f8ac687de1d803e1d6885b184db83540
LuthandoNgombane <luthandongombane@gmail.com>
2026-01-23 22:00:25 +0200

    Fix

--- frontend/fynapay-mobile/app/_layout.tsx
+++ frontend/fynapay-mobile/app/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from 'expo-router';
 
 export default function Layout() {
   return (
-    // "headerShown: false" hides the default top bar so your custom UI looks better
+    //LN -  "headerShown: false" hides the default top bar so your custom UI looks better
     <Stack screenOptions={{ headerShown: false }} />
   );
 }
